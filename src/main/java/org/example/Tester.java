11efb1f8ae81a9588aa8e4525f8b4c226d50e837
Gleb <Gleb.by2005@gmail.com>
2024-04-04 10:17:07 +0300

    Commit

--- src/main/java/org/example/Tester.java
+++ src/main/java/org/example/Tester.java
@@ -29,6 +29,7 @@ public class Tester
 
     public void printInfo()
     {
+
         System.out.println("Name: " + name);
         System.out.println("Surname: " + surname);
         System.out.println("Experience in years: " + experienceInYears);
